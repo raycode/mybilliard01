784464f485f3824f6ba0d6c8f38dbd585d721dd3
wrice127 <wrice127@af801a76-7f76-11de-8b9f-9be6f49bd635>
2009-08-14 02:10:00 +0000

    Adding NullRenderBufferFactoryDX9.

--- render_win32/my_render_win32_dx9.h
+++ render_win32/my_render_win32_dx9.h
@@ -10,14 +10,17 @@ namespace my_render_win32_dx9 {
     class VertexBufferDX9;
     class IndexBufferDX9;
     class RenderBufferFactoryDX9;
+    class NullRenderBufferFactoryDX9;
     class RednerWin32DX9;
 
     MY_SMART_PTR( VertexBufferDX9 );
     MY_SMART_PTR( IndexBufferDX9 );
     MY_SMART_PTR( RenderBufferFactoryDX9 );
+    MY_SMART_PTR( NullRenderBufferFactoryDX9 );
 }
 
 #include "RenderBufferFactoryDX9.h"
+#include "NullRenderBufferFactoryDX9.hpp"
 #include "VertexBufferDX9.h"
 #include "IndexBufferDX9.h"
 #include "RenderWin32DX9.h"
