bf0a70cc95b6b4835f063b287e8f212688abc092
wrice127 <wrice127@af801a76-7f76-11de-8b9f-9be6f49bd635>
2009-08-21 08:10:54 +0000

    View matrix is fixed again.

--- trunk/mybilliard01/MyCamera.cpp
+++ trunk/mybilliard01/MyCamera.cpp
@@ -1,6 +1,7 @@
 #include "stdafx.h"
 #include "my_app.h"
 
+
 MyCamera::MyCamera( Camera * cameraCollada, MyPhysX * phys,
                    NxVec3 initPosition, NxVec3 direction, bool bRightHand )
 : colladaCamera_( cameraCollada )
@@ -10,8 +11,63 @@ MyCamera::MyCamera( Camera * cameraCollada, MyPhysX * phys,
     controller_ = phys_->addCapsuleCharacter( initPosition, 0.1f, 0.2f, 0.001f, NX_Z );
 
     NxVec3 up = NxVec3( 0.f, 0.f, 1.f );
-    NxVec3 right = (bRightHand ? up.cross( direction ) : direction.cross( up ) );
+    NxVec3 right = ( bRightHand ? up.cross( direction ) : direction.cross( up ) );
     rotate_ = NxMat33( right, up, direction );
+}
+
+NxU32 MyCamera::move( NxReal x, NxReal y, NxReal z, NxReal elapsedTime )
+{
+    NxU32 collisionGroups = 0u;//COLLIDABLE_MASK;
+    NxVec3 dispVector( x, y, z );
+
+    //	NxF32 sharpness = 0.1f;
+    NxF32 sharpness = 1.0f;
+
+    const NxVec3 d = dispVector * elapsedTime;
+
+    NxU32 collisionFlags;
+    controller_->move( d, collisionGroups, 0.000001f, collisionFlags, sharpness);
+    return collisionFlags;
+}
+
+void MyCamera::getViewMatrix44( float * returnMatrix44, bool bRowMajor )
+{
+    NxVec3 dir = getDirectionVector();
+    dir.normalize();
+
+    NxVec3 up = getRightVector().cross( dir );
+    up.normalize();
+
+    NxVec3 right = dir.cross( up );
+    right.normalize();
+
+    NxVec3 trans;
+    trans.x = (NxReal) - getPosition().dot( right );
+    trans.y = (NxReal) - getPosition().dot( up );
+    trans.z = (NxReal) - getPosition().dot( dir );
+
+    NxMat34 rowMajorView;
+    rowMajorView.M.setRow( 0, right );
+    rowMajorView.M.setRow( 1, up );
+    rowMajorView.M.setRow( 2, dir );
+    rowMajorView.t = trans;
+
+    if( bRowMajor ) {
+        rowMajorView.getRowMajor44( returnMatrix44 );
+
+    } else {
+        rowMajorView.getColumnMajor44( returnMatrix44 );
+    }
+}
+
+NxVec3 MyCamera::getRightVector () const {
+    return rotate_.getRow( 0 );
+}
+NxVec3 MyCamera::getUpVector () const {
+    return rotate_.getRow( 1 );
+}
+NxVec3 MyCamera::getDirectionVector () const {
+    return rotate_.getRow( 2 );
 }
 
 void MyCamera::setAspect( float aspectRatio )
@@ -26,74 +82,6 @@ void MyCamera::getProjectionMatrix44( float * returnMatrix44, bool bRightHand, b
 const NxExtendedVec3 & MyCamera::getPosition()
 {
     return controller_->getFilteredPosition();
-}
-
-
-NxVec3 MyCamera::getRightVector () const {
-    return rotate_.getRow( 0 );
-}
-NxVec3 MyCamera::getUpVector () const {
-    return rotate_.getRow( 1 );
-}
-NxVec3 MyCamera::getDirectionVector () const {
-    return rotate_.getRow( 2 );
-}
-
-void MyCamera::getViewMatrix44( float * returnMatrix44, bool bRowMajor )
-{
-    NxVec3 dir = getDirectionVector();
-    dir.normalize();
-
-    NxVec3 up = (bRightHand_ ? getRightVector().cross( dir ) : dir.cross( getRightVector() ) );
-    up.normalize();
-
-    NxVec3 right = (bRightHand_ ? dir.cross( up ) : up.cross( dir ) );
-    right.normalize();
-
-    NxVec3 trans;
-    trans.x = (NxReal) - getPosition().dot( right );
-    trans.y = (NxReal) - getPosition().dot( up );
-    trans.z = (NxReal) - getPosition().dot( dir );
-
-    NxMat34 rowMajorView;
-    rowMajorView.M.setRow( 0, right * ( bRightHand_ ? 1.f : -1.f ) );
-    rowMajorView.M.setRow( 1, up * ( bRightHand_ ? 1.f : -1.f ) );
-    rowMajorView.M.setRow( 2, dir * ( bRightHand_ ? 1.f : -1.f ) );
-    rowMajorView.t = trans;
-
-    if( bRowMajor ) {
-        rowMajorView.getRowMajor44( returnMatrix44 );
-
-    } else {
-        rowMajorView.getColumnMajor44( returnMatrix44 );
-    }
-}
-
-NxU32 MyCamera::move( NxReal x, NxReal y, NxReal z, NxReal elapsedTime )
-{
-    NxU32 collisionGroups = 0u;//COLLIDABLE_MASK;
-    NxVec3 dispVector( x, y, z );
-
-    //	NxF32 sharpness = 0.1f;
-    NxF32 sharpness = 1.0f;
-
-    const NxVec3 d = dispVector * elapsedTime;
-
-    NxU32 collisionFlags;
-    controller_->move( -d, collisionGroups, 0.000001f, collisionFlags, sharpness);
-    return collisionFlags;
-}
-
-void MyCamera::rotateX( NxReal angle ) {
-
-}
-
-void MyCamera::rotateY( NxReal angle ) {
-
-}
-
-void MyCamera::rotateZ( NxReal angle ) {
-
 }
 
 
--- trunk/mybilliard01/MyInputListenerImp.cpp
+++ trunk/mybilliard01/MyInputListenerImp.cpp
@@ -6,8 +6,14 @@
 #define KEY_RIGHT_ARROW 39
 #define KEY_DOWN_ARROW 40
 
-MyInputListenerImp::MyInputListenerImp( MyRenderEventListenerImp * renderListener ) {
+MyInputListenerImp::MyInputListenerImp( MyRenderEventListenerImp * renderListener )
+: m_bDrag( false )
+{
     renderListener_ = renderListener;
+    D3DXQuaternionIdentity( &m_qDown );
+    D3DXQuaternionIdentity( &m_qNow );
+    m_vDownPt = D3DXVECTOR3( 0, 0, 0 );
+    m_vCurrentPt = D3DXVECTOR3( 0, 0, 0 );
 }
 
 void MyInputListenerImp::keyDown( unsigned int key, bool bAlt ) {
@@ -60,25 +66,6 @@ void MyInputListenerImp::onMouseEvent(
     if( bLeftButton ) selectBall( xPos, yPos );
 }
 
-bool MyInputListenerImp::MsgProc( HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam ) {
-    return true;
-}
-
-MyCamera * MyInputListenerImp::getCamera()
-{
-    return renderListener_->getMyCamera();
-}
-
-void MyInputListenerImp::rotateCamera( int xPos, int yPos )
-{
-
-}
-
-void MyInputListenerImp::selectBall( int xPos, int yPos )
-{
-
-}
-
 void MyInputListenerImp::beginMoveForward()
 {
     //getCamera()->move(  )
@@ -112,4 +99,109 @@ void MyInputListenerImp::endMoveBackward()
 
 }
 
+
+bool MyInputListenerImp::MsgProc( HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam )
+{
+#ifdef AAA
+    // Current mouse position
+    int iMouseX = ( short )LOWORD( lParam );
+    int iMouseY = ( short )HIWORD( lParam );
+
+    switch( uMsg )
+    {
+    case WM_LBUTTONDOWN:
+    case WM_LBUTTONDBLCLK:
+        SetCapture( hWnd );
+        OnBegin( iMouseX, iMouseY );
+        return TRUE;
+
+    case WM_LBUTTONUP:
+        ReleaseCapture();
+        OnEnd();
+        return TRUE;
+    case WM_CAPTURECHANGED:
+        if( ( HWND )lParam != hWnd )
+        {
+            ReleaseCapture();
+            OnEnd();
+        }
+        return TRUE;
+
+    case WM_MOUSEMOVE:
+        if( MK_LBUTTON & wParam )
+        {
+            OnMove( iMouseX, iMouseY );
+        }
+        return TRUE;
+    }
+#endif
+    return true;
+}
+
+void MyInputListenerImp::OnBegin( int nX, int nY )
+{
+    m_bDrag = true;
+    m_qDown = m_qNow;
+    m_vDownPt = ScreenToVector( ( float )nX, ( float )nY );
+}
+
+void MyInputListenerImp::OnMove( int nX, int nY )
+{
+    if( m_bDrag )
+    {
+        m_vCurrentPt = ScreenToVector( ( float )nX, ( float )nY );
+        //m_qNow = m_qDown * QuatFromBallPoints( m_vDownPt, m_vCurrentPt );
+    }
+}
+
+void MyInputListenerImp::OnEnd()
+{
+    m_bDrag = false;
+}
+
+D3DXVECTOR3 MyInputListenerImp::ScreenToVector( float fScreenPtX, float fScreenPtY )
+{
+    // Scale to screen
+    FLOAT x = -( fScreenPtX - m_Offset.x - m_nWidth / 2 ) / ( m_fRadius * m_nWidth / 2 );
+    FLOAT y = ( fScreenPtY - m_Offset.y - m_nHeight / 2 ) / ( m_fRadius * m_nHeight / 2 );
+
+    FLOAT z = 0.0f;
+    FLOAT mag = x * x + y * y;
+
+    if( mag > 1.0f )
+    {
+        FLOAT scale = 1.0f / sqrtf( mag );
+        x *= scale;
+        y *= scale;
+    }
+    else
+        z = sqrtf( 1.0f - mag );
+
+    // Return vector
+    return D3DXVECTOR3( x, y, z );
+}
+
+//D3DXQUATERNION CD3DArcBall::QuatFromBallPoints( const D3DXVECTOR3& vFrom, const D3DXVECTOR3& vTo )
+//{
+//    D3DXVECTOR3 vPart;
+//    float fDot = D3DXVec3Dot( &vFrom, &vTo );
+//    D3DXVec3Cross( &vPart, &vFrom, &vTo );
+//
+//    return D3DXQUATERNION( vPart.x, vPart.y, vPart.z, fDot );
+//}
+
+MyCamera * MyInputListenerImp::getCamera()
+{
+    return renderListener_->getMyCamera();
+}
+
+void MyInputListenerImp::rotateCamera( int xPos, int yPos )
+{
+
+}
+
+void MyInputListenerImp::selectBall( int xPos, int yPos )
+{
+
+}
 
