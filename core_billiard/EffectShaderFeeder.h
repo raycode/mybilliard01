86f5e7fe19958de861d4d705a47650677a3967ff
wrice127 <wrice127@af801a76-7f76-11de-8b9f-9be6f49bd635>
2009-09-05 07:10:50 +0000

    Modifying effect shader feeder interface.

--- core_billiard/EffectShaderFeeder.h
+++ core_billiard/EffectShaderFeeder.h
@@ -5,18 +5,20 @@ namespace my_render {
 MY_INTERFACE EffectShaderFeeder {
     virtual ~EffectShaderFeeder() {}
 
-    virtual void displayWithEffect() PURE;
+    virtual void setEffectShader( EffectShader * ) PURE;
+    virtual EffectShader * getEffectShader() PURE;
 
-    virtual void updateProjection( const RowMajorMatrix44f & matProj ) PURE;
+    virtual void updateCameraProjection( const RowMajorMatrix44f & matProj ) PURE;
 
     virtual void updateCameraMatrix(
         const NxVec3 & cameraPos,
         const NxVec3 & cameraDir,
-        const RowMajorMatrix44f & matWorld,
         const RowMajorMatrix44f & matView,
         const RowMajorMatrix44f & matProjView ) PURE;
 
-    virtual EffectShader * getEffectShader() PURE;
+    virtual void updateModelMatrix( const RowMajorMatrix44f & matWorld ) PURE;
+
+    virtual bool displayWithEffect( EffectShaderCallBack * calBack ) PURE;
 
 };
 
